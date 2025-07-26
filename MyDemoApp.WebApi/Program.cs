22d9a658cfad634441038bfd4481a77cde064039
shamyratpashiyev96 <shamyratpashiyev96@gmail.com>
2025-07-26 14:17:08 +0500

    Created backend webApi app and connected it to geminiApi

---
+++ MyDemoApp.WebApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using MyDemoApp.WebApi.Data;
+using MyDemoApp.WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<GeminiService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.MapOpenApi();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+
+
+app.MapPost("/gemini", async (GeminiService geminiService, string prompt) =>
+{
+    var result = await geminiService.GenerateTextAsync(prompt);
+    return Results.Ok(result);
+});
+
+app.Run();
---
+++ MyDemoApp.WebApi/Services/GeminiService.cs
@@ -0,0 +1,51 @@
+using GeminiDotnet;
+using GeminiDotnet.ContentGeneration;
+using Microsoft.Extensions.AI;
+
+namespace MyDemoApp.WebApi.Services;
+
+public class GeminiService
+{
+    private readonly GeminiClient _geminiClient;
+
+    public GeminiService(IConfiguration configuration)
+    {
+        var apiKey = configuration["Gemini:ApiKey"];
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            throw new InvalidOperationException("Gemini API key is not configured.");
+        }
+        var options = new GeminiClientOptions
+        {
+            ApiKey = apiKey
+        };
+        _geminiClient = new GeminiClient(options);
+    }
+
+    public async Task<string> GenerateTextAsync(string prompt)
+    {
+        var request = new GenerateContentRequest()
+        {
+            Contents =
+            [
+              new Content()
+              {
+                Parts = [new Part(){ Text = prompt }]
+              }
+            ]
+        };
+        var response = "";
+        await foreach (var result in _geminiClient.GenerateContentStreamingAsync("gemini-2.0-flash", request))
+        {
+          foreach (var candidate in result.Candidates)
+          {
+            foreach (var part in candidate.Content.Parts)
+            {
+              response += part.Text;
+            }
+          }
+        }
+
+        return response;
+    }
+}
